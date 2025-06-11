18d4bef748ea5219c480fd01e708e5369ec6fd68
irvan maulana <irvanm166@gmail.com>
2025-06-11 22:05:20 +0700

    Menampilkan gambar dalam grid

--- app/src/main/java/com/irvanmaulana0013/barangku/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/irvanmaulana0013/barangku/ui/screen/MainViewModel.kt
@@ -1,13 +1,18 @@
 package com.irvanmaulana0013.barangku.ui.screen
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.irvanmaulana0013.barangku.model.Barang
 import com.irvanmaulana0013.barangku.network.BarangApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    var data = mutableStateOf(emptyList<Barang>())
+        private set
 
     init {
         retrieveData()
@@ -16,8 +21,7 @@ class MainViewModel : ViewModel() {
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = BarangApi.service.getBarang("__admin__")
-                Log.d("MainViewModel", "Success: $result")
+                data.value = BarangApi.service.getBarang("__admin__")
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
