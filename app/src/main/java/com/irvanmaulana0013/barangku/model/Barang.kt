2e20c24502a14556cef4305f7d2e5fb0d04e8ef6
irvan maulana <irvanm166@gmail.com>
2025-06-11 21:34:42 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/irvanmaulana0013/barangku/model/Barang.kt
@@ -0,0 +1,9 @@
+package com.irvanmaulana0013.barangku.model
+
+data class Barang(
+    val id: Int,
+    val imageUrl: String,
+    val jumlah: Int,
+    val kategori: String,
+    val namaBarang: String
+)
--- app/src/main/java/com/irvanmaulana0013/barangku/network/BarangApiService.kt
+++ app/src/main/java/com/irvanmaulana0013/barangku/network/BarangApiService.kt
@@ -1,14 +1,21 @@
 package com.irvanmaulana0013.barangku.network
 
+import com.irvanmaulana0013.barangku.model.Barang
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 private const val BASE_URL = "https://fd23-160-19-227-86.ngrok-free.app/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -16,7 +23,7 @@ interface BarangApiService {
     @GET("barangku")
     suspend fun getBarang(
         @Header("Authorization") userId: String
-    ): String
+    ): List<Barang>
 }
 
 object BarangApi {
